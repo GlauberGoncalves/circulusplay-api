48c11e80058eedc45fd62604f7afa419a12f8b9a
GlauberGoncalves <sistemas.glauber@gmail.com>
2019-06-23 00:23:08 -0300

    finalização do feed de filmes do usuario e amigos

--- src/main/java/br/com/glauber/circulusplay/dto/FeedDto.java
+++ src/main/java/br/com/glauber/circulusplay/dto/FeedDto.java
@@ -1,5 +1,7 @@
 package br.com.glauber.circulusplay.dto;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,12 +11,13 @@ public class FeedDto {
 
 	private UsuarioDto usuario;
 	private List<UsuarioDto> amigos;
-	private List<PostagemDto> postagens;
+	private List<PostagemDto> postagens = null;
 
 	public FeedDto(Usuario usuario) {
 		this.usuario = new UsuarioDto(usuario);
 		this.amigos = usuario.getAmigos().stream().map(obj -> new UsuarioDto(obj)).collect(Collectors.toList());
 		this.postagens = usuario.getPostagens().stream().map(PostagemDto::new).collect(Collectors.toList());
+		this.ordenaPostagens();
 	}
 
 	public UsuarioDto getUsuario() {
@@ -27,6 +30,26 @@ public class FeedDto {
 
 	public List<PostagemDto> getPostagens() {
 		return postagens;
+	}
+	
+	public void ordenaPostagens() {
+		Comparator<PostagemDto> cmp = new Comparator<PostagemDto>() {
+
+			@Override
+			public int compare(PostagemDto o1, PostagemDto o2) {
+				if (o1.getInstante().before(o2.getInstante())) {
+					return 1;
+				} else if (o1.getInstante().after(o2.getInstante())) {
+					return -1;
+				} else {
+					return 0;
+				}				
+			}
+		};
+		
+		if( this.postagens != null) {
+			Collections.sort(this.postagens, cmp);			
+		}
 	}
 
 }
